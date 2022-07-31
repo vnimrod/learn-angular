58b1ee98214f5a56ecd1535455d880f9f2226685
nimrod vain <togithub1@gmail.com>
2022-07-31 22:47:31 +0300

    create our own structural directive that check the oppsite of *ngIf -> if not (!x)

--- directives/src/app/unless.directive.ts
+++ directives/src/app/unless.directive.ts
@@ -1,17 +1,24 @@
 import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
+// This directive simulate *ngIf in the opposite way, it check - if not (!x)
 @Directive({
-  selector: '[appUnless]'
+  selector: '[appUnless]',
 })
 export class UnlessDirective {
+  // appUnless is a property, it just a setter of the property which is a method that get executed whenever the property changes. (outside of this directive)
+  // appUnless name should be the same as the directive.
   @Input() set appUnless(condition: boolean) {
     if (!condition) {
+      // creates a view in this view container
       this.vcRef.createEmbeddedView(this.templateRef);
     } else {
       this.vcRef.clear();
     }
   }
 
-  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
-
+  constructor(
+    // the template is the what, and vcRef(view container) is the where.
+    private templateRef: TemplateRef<any>,
+    private vcRef: ViewContainerRef
+  ) {}
 }
