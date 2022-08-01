1409d7d324828746dbd8f15efb8a13d21d6e5a49
nimrod vain <togithub1@gmail.com>
2022-08-01 21:30:06 +0300

    using @Injectable decorator to use service in other service

--- services/src/app/accounts.service.ts
+++ services/src/app/accounts.service.ts
@@ -5,6 +5,7 @@ import { LoggingService } from './logging.service';
 // When we use a service on our component, all his child components have access to the properties in that service.
 // If we provide a service on AppModule, instances of that service will be available Application wide.
 
+// Injectable decorator tell angular that to this service something can be inject, the receiving service. (here we using loggingService)
 @Injectable()
 export class AccountsService {
   accounts = [
@@ -23,6 +24,7 @@ export class AccountsService {
   ];
   statusUpdated = new EventEmitter<string>();
 
+  // inject LoggingService to AccountsService
   constructor(private loggingService: LoggingService) {}
 
   addAccount(name: string, status: string) {
