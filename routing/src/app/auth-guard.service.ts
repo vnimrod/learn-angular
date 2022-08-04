6318755380bc2624fe4e505d47f45745c5e376b7
nimrod vain <togithub1@gmail.com>
2022-08-04 22:59:26 +0300

    init routing starter project

---
+++ routing/src/app/auth-guard.service.ts
@@ -0,0 +1,36 @@
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+  CanActivateChild,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  async canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Promise<boolean> {
+    const authenticated = await this.authService.isAuthenticated();
+    if (authenticated) {
+      return true;
+    } else {
+      this.router.navigate(['/']);
+      return false;
+    }
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Promise<boolean> {
+    return this.canActivate(route, state);
+  }
+}
