c4053a27eb520a44789bed35d972938d6d2f96bb
nimrod vain <togithub1@gmail.com>
2022-08-06 15:15:42 +0300

    change comment

--- routing-project/src/app/home/home.component.ts
+++ routing-project/src/app/home/home.component.ts
@@ -14,7 +14,9 @@ export class HomeComponent implements OnInit {
   ngOnInit() {}
 
   onLoadServer(id: number) {
-    // complex calculation
+    /* 
+      ...complex calculation 
+    */
     this.router.navigate(["/servers", id, "edit"], {
       queryParams: { allowEdit: "1" },
       fragment: "loading",
