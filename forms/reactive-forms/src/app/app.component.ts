d31767d22723d54d8ee9e475a8a665c302f197f9
nimrod vain <togithub1@gmail.com>
2022-08-16 20:02:09 +0300

    reactive forms - create new FormGroup and FormControl

--- forms/reactive-forms/src/app/app.component.ts
+++ forms/reactive-forms/src/app/app.component.ts
@@ -1,46 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Observable } from 'rxjs/Observable';
+import { Component, OnInit } from "@angular/core";
+import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  genders = ['male', 'female'];
+  genders = ["male", "female"];
   signupForm: FormGroup;
-  forbiddenUsernames = ['Chris', 'Anna'];
+  forbiddenUsernames = ["Chris", "Anna"];
 
   constructor() {}
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'userData': new FormGroup({
-        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
+      // keys below are controls
+      userData: new FormGroup({
+        /* FormControl arguments:
+           1. First argument of FromControl (null) is the initial state, value of this control.
+           2. Second argument will be a single or array of validators.
+           3. Third argument asynchronous validator.
+        */
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
+        email: new FormControl(
+          null,
+          [Validators.required, Validators.email],
+          this.forbiddenEmails
+        ),
       }),
-      'gender': new FormControl('male'),
-      'hobbies': new FormArray([])
+      gender: new FormControl("male"),
+      hobbies: new FormArray([]),
     });
     // this.signupForm.valueChanges.subscribe(
     //   (value) => console.log(value)
     // );
-    this.signupForm.statusChanges.subscribe(
-      (status) => console.log(status)
-    );
+    this.signupForm.statusChanges.subscribe((status) => console.log(status));
     this.signupForm.setValue({
-      'userData': {
-        'username': 'Max',
-        'email': 'max@test.com'
+      userData: {
+        username: "Max",
+        email: "max@test.com",
       },
-      'gender': 'male',
-      'hobbies': []
+      gender: "male",
+      hobbies: [],
     });
     this.signupForm.patchValue({
-      'userData': {
-        'username': 'Anna',
-      }
+      userData: {
+        username: "Anna",
+      },
     });
   }
 
@@ -51,12 +62,12 @@ export class AppComponent implements OnInit {
 
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
-    (<FormArray>this.signupForm.get('hobbies')).push(control);
+    (<FormArray>this.signupForm.get("hobbies")).push(control);
   }
 
-  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
     if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
-      return {'nameIsForbidden': true};
+      return { nameIsForbidden: true };
     }
     return null;
   }
@@ -64,8 +75,8 @@ export class AppComponent implements OnInit {
   forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
     const promise = new Promise<any>((resolve, reject) => {
       setTimeout(() => {
-        if (control.value === 'test@test.com') {
-          resolve({'emailIsForbidden': true});
+        if (control.value === "test@test.com") {
+          resolve({ emailIsForbidden: true });
         } else {
           resolve(null);
         }
--- forms/reactive-forms/src/app/app.module.ts
+++ forms/reactive-forms/src/app/app.module.ts
@@ -1,19 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule
-  ],
+  declarations: [AppComponent],
+  // need to import ReactiveFormsModule
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
